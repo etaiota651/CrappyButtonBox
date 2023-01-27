fd28a595af97c30aa5a26bc086b85e57abfd2500
John DiGiacobbe <61557727+etaiota651@users.noreply.github.com>
2023-01-27 16:55:30 -0500

    Update button_box.ino

--- button_box.ino
+++ button_box.ino
@@ -8,7 +8,7 @@ const int colPin[] = {7,8,9,10,11};
 const int bigSwitch[] = {12,13};
 
 //This button list may need to be adjusted based
-//upon what buttons I want to be what. 
+//upon what buttons I want to be what.
 //Actually, I'm sure of it.
 const int buttonList[5][5] = {{1,2,3,4,5},
                             {6,7,8,9,10},
@@ -33,7 +33,8 @@ unsigned long lastBounce[5][5] = {{0,0,0,0,0},
 
 unsigned long bounceTime = 0;
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_MULTI_AXIS,26,0,false,false,false,true,true,true,false,false,false,false,false);
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_MULTI_AXIS,26,
+          0,false,false,false,true,true,true,false,false,false,false,false);
 
 
 void setup() {
@@ -57,15 +58,16 @@ void loop() {
     digitalWrite(rowPin[i], HIGH);
     for(int j=0; j < sizeof(colPin); j++){
       int currentButtonState = digitalRead(colPin[j])
-      if(digitalRead(colPin[j]) == LOW){
+      if(currentButtonState == LOW){
         Joystick.pressButton(buttonList[i][j]);
-        
+
         //Presently just set up as a way to test things. Need to figure out how to change states
-        //over time, so buttons can be held. Want to send individual PRESS and RELEASE events 
+        //over time, so buttons can be held. Want to send individual PRESS and RELEASE events
         //when they actually happen.
         delay(100);
         Joystick.releaseButton(buttonList[i][j]);
       }
     }
+    digitalWrite(rowPin[i], LOW);
   }
 }
