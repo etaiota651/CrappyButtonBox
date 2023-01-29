ce743b6099c52f8238199accdf383a643f1c5f19
John DiGiacobbe <61557727+etaiota651@users.noreply.github.com>
2023-01-29 10:39:13 -0500

    Update button_box.ino

--- button_box.ino
+++ button_box.ino
@@ -22,20 +22,27 @@ int buttonState[5][5] = {{0,0,0,0,0},
                         {0,0,0,0,0},
                         {0,0,0,0,0}};
 
-unsigned long lastBounce[5][5] = {{0,0,0,0,0},
-                                  {0,0,0,0,0},
-                                  {0,0,0,0,0},
-                                  {0,0,0,0,0},
-                                  {0,0,0,0,0}};
+//unsigned long lastBounce[5][5] = {{0,0,0,0,0},
+//                                  {0,0,0,0,0},
+//                                  {0,0,0,0,0},
+//                                  {0,0,0,0,0},
+//                                  {0,0,0,0,0}};
 
 #define ROT_MIN 0
 #define ROT_MAX 1023
+#define BOUNCE_INTERVAL 50
+
 
 unsigned long bounceTime = 0;
+unsigned long lastBounce = 0;
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_MULTI_AXIS,26,
           0,false,false,false,true,true,true,false,false,false,false,false);
 
+//TODO: Get bounce working. Thinking of only polling switches once per\
+//    BOUNCE_INTERVAL, instead of keeping track of when each button was pushed.
+//    Might wind up being less than optimal, but if it doesn't work I can always
+//    write a more granular anti-bounce.
 
 void setup() {
   //set initial pin states from pin arrays
@@ -52,22 +59,31 @@ void setup() {
   Joystick.setRzAxisRange(ROT_MIN, ROT_MAX);
 }
 
+
+//TODO:
+//  Set up states for checking previous button state, and send a pressButton or releaseButton
+//  event based upon the previous state. Then update the previous state with the current one.
+
 void loop() {
   bounceTime = millis();
-  for(int i=0; i < sizeof(rowPin); i++){
-    digitalWrite(rowPin[i], HIGH);
-    for(int j=0; j < sizeof(colPin); j++){
-      int currentButtonState = digitalRead(colPin[j])
-      if(currentButtonState == LOW){
-        Joystick.pressButton(buttonList[i][j]);
+  if(bounceTime - lastBounce >= BOUNCE_INTERVAL){
+    for(int i=0; i < sizeof(rowPin); i++){
+      digitalWrite(rowPin[i], HIGH);
+      for(int j=0; j < sizeof(colPin); j++){
+        int currentButtonState = digitalRead(colPin[j]);
+        if(currentButtonState == LOW){
+          Joystick.pressButton(buttonList[i][j]);
 
-        //Presently just set up as a way to test things. Need to figure out how to change states
-        //over time, so buttons can be held. Want to send individual PRESS and RELEASE events
-        //when they actually happen.
-        delay(100);
-        Joystick.releaseButton(buttonList[i][j]);
+          //Presently just set up as a way to test things. Need to figure out how to change states
+          //over time, so buttons can be held. Want to send individual PRESS and RELEASE events
+          //when they actually happen.
+          delay(100);
+          Joystick.releaseButton(buttonList[i][j]);
+        }
       }
-    }
     digitalWrite(rowPin[i], LOW);
+
+    }
+    lastBounce = bounceTime;
   }
 }
