c2ce3325b6681f27d77d12af5d575b4ef222d7b8
John DiGiacobbe <61557727+etaiota651@users.noreply.github.com>
2023-01-31 16:37:54 -0600

    Update button_box.ino

--- button_box.ino
+++ button_box.ino
@@ -1,6 +1,7 @@
 #include <Joystick.h>
 
-//These pins probably aren't correct. Need to do some trial and error with the board.
+//These pins probably aren't correct.
+//Need to do some trial and error with the board.
 //Also, row and col might need to be reversed. This will all become clear
 //when I get everything actually hooked up to the board.
 const int rowPin[] = {2,3,4,5,6};
@@ -10,11 +11,17 @@ const int bigSwitch[] = {12,13};
 //This button list may need to be adjusted based
 //upon what buttons I want to be what.
 //Actually, I'm sure of it.
-const int buttonList[5][5] = {{1,2,3,4,5},
-                            {6,7,8,9,10},
-                            {11,12,13,14,15},
-                            {16,17,18,19,20},
-                            {21,22,23,24,25}};
+const int buttonList[5][5] = {{0,1,2,3,4},
+                            {5,6,7,8,9},
+                            {10,11,12,13,14},
+                            {15,16,17,18,19},
+                            {20,21,22,23,99}};
+const int bigSwitchButtonAssignment[2] = {24, 25};
+
+#define ROT_MIN 0
+#define ROT_MAX 1023
+#define BOUNCE_INTERVAL 50
+#define ANALOG_BOUNCE_INTERVAL 20 //may not need this
 
 int buttonState[5][5] = {{0,0,0,0,0},
                         {0,0,0,0,0},
@@ -22,22 +29,13 @@ int buttonState[5][5] = {{0,0,0,0,0},
                         {0,0,0,0,0},
                         {0,0,0,0,0}};
 
-//unsigned long lastBounce[5][5] = {{0,0,0,0,0},
-//                                  {0,0,0,0,0},
-//                                  {0,0,0,0,0},
-//                                  {0,0,0,0,0},
-//                                  {0,0,0,0,0}};
-
-#define ROT_MIN 0
-#define ROT_MAX 1023
-#define BOUNCE_INTERVAL 50
-
+unsigned int axisValue[4] = {0,0,0,0};
 
 unsigned long currentMillis = 0;
 unsigned long lastBounce = 0;
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_MULTI_AXIS,26,
-          0,false,false,false,true,true,true,false,false,false,false,false);
+          0,false,false,true,true,true,true,false,false,false,false,false);
 
 //TODO: Get bounce working. Thinking of only polling switches once per\
 //    BOUNCE_INTERVAL, instead of keeping track of when each button was pushed.
@@ -59,17 +57,23 @@ void setup() {
   Joystick.setRxAxisRange(ROT_MIN, ROT_MAX);
   Joystick.setRyAxisRange(ROT_MIN, ROT_MAX);
   Joystick.setRzAxisRange(ROT_MIN, ROT_MAX);
-  Joystick.setYAxisRange(ROT_MIN, ROT_MAX); //have to use a linear axis for rot4
+  Joystick.setZAxisRange(ROT_MIN, ROT_MAX); //have to use a linear axis for rot4
 
 }
-
 
 //TODO:
 //  Set up states for checking previous button state, and send a pressButton or releaseButton
 //  event based upon the previous state. Then update the previous state with the current one.
 
 void loop() {
+  //The thought here is to check for a time delta before checking button states.
+  //This SHOULD mitigate any bounces.
   currentMillis = millis();
+  str pressedButtonList = "";
+
+  //Check if interval is good, then step through the row pin, activate the row,
+  //then step through the column pins and check for a signal.
+  //This will then update the button state array based upon the read data.
   if(currentMillis - lastBounce >= BOUNCE_INTERVAL)
   {
     for(int i=0; i < sizeof(rowPin); i++)
@@ -78,18 +82,35 @@ void loop() {
       for(int j=0; j < sizeof(colPin); j++)
       {
         int currentButtonState = digitalRead(colPin[j]);
-        if(currentButtonState == LOW && buttonState[i][j] == 1)
+
+        if(currentButtonState == LOW && buttonState[i][j] == 0)
         {
           Joystick.pressButton(buttonList[i][j]);
-          buttonState[i][j] = 0;}
-        else if(currentButtonState == HIGH && buttonState[i][j] == 0)
+          buttonState[i][j] = 1;
+          pressedButtonList = pressedButtonList + buttonList[i][j];
+        }
+        else if(currentButtonState == HIGH && buttonState[i][j] == 1)
         {
           Joystick.releaseButton(buttonList[i][j]);
-          buttonState[i][j] = 1;
+          buttonState[i][j] = 0;
         }
+
       }
     //digitalWrite(rowPin[i], LOW);
     }
+    for(int p=0; p < sizeof(bigSwitch); p++)
+    {
+     
+    }
     lastBounce = currentMillis;
   }
+  //Set all analog axes. This may need bounce protection, as well,
+  //expecially if the pots are noisy. Hell, I may need to set some guard rails
+  //regarding change, but I'd rather not if I don't have to.
+  Joystick.setRxAxis(analogRead(A0));
+  Joystick.setRyAxis(analogRead(A1));
+  Joystick.setRzAxis(analogRead(A2));
+  Joystick.setZAxis(analogread(A3));
+
+  Serial.println(pressedButtonList);
 }
